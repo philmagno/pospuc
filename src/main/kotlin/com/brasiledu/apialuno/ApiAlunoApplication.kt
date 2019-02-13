36721a1fc50eb210a1f8a214bcbc70654a67982b
philippemagno <philippe.magno@hotmart.com.br>
2019-02-13 16:53:23 -0200

    Initial comit

---
+++ src/main/kotlin/com/brasiledu/apialuno/ApiAlunoApplication.kt
@@ -0,0 +1,12 @@
+package com.brasiledu.apialuno
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiAlunoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiAlunoApplication>(*args)
+}
+
