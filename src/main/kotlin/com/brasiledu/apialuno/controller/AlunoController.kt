9fd2deb648a24d6b071717820177a056b46419d9
Philippe Magno <philippe.magno@hotmart.com>
2019-02-25 21:33:48 -0300

    Roles in Aluno Controller

--- src/main/kotlin/com/brasiledu/apialuno/controller/AlunoController.kt
+++ src/main/kotlin/com/brasiledu/apialuno/controller/AlunoController.kt
@@ -4,6 +4,7 @@ import com.brasiledu.apialuno.dto.AlunoDTO
 import com.brasiledu.apialuno.model.Aluno
 import com.brasiledu.apialuno.service.AlunoService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.*
 class AlunoController @Autowired constructor(var alunoService: AlunoService){
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     fun listAll() = alunoService.list()
 
     @PostMapping(consumes = ["application/json"])
+    @PreAuthorize("hasRole('ADMIN') OR hasRole('USER')")
     fun saveAluno(@RequestBody usuario: AlunoDTO) = alunoService.save(usuario)
 
     @GetMapping("/:alunoId")
+    @PreAuthorize("hasRole('ADMIN') OR hasRole('USER')")
     fun userInfo(@PathVariable alunoId: Long) = alunoService.consultarDadosPessoais(alunoId)
 
 }
